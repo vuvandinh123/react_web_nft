5ebd6362f6622b8c4a9d3b91c0fb51e9ef617226
vuvandinh123 <vudinh.01633583800@gmail.com>
2023-07-20 12:58:54 +0700

    tets

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import './App.scss'
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layout/layout'
 import HomePage from './pages/HomePage/HomePage';
+import CardList from './components/Gloabal/CardList';
 function App() {
 
   return (
     <>
-       <HashRouter>
-      <Switch>
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
         </Route>
-      </Switch>
-    </HashRouter>
+      </Routes>
+      <CardList/>
     </>
   )
 }
