1741592ffc868568a0139fa16299d3ddcf9c8fa3
hongthom28102003 <thomhong102003@gmail.com>
2024-01-12 13:35:35 +0700

    update base

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/vvd-nft', 
+  base: "/react_web_nft",
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-})
+});
