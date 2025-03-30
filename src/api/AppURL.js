1b02848d78e480aa35f63ffd0ed44c43588a441f
vuvandinh123 <vuvandinh203@gmail.com>
2025-03-30 16:11:24 +0700

    fix: .env

--- src/api/AppURL.js
+++ src/api/AppURL.js
@@ -1,4 +1,4 @@
 export class AppURL {
-    static BaseURL = import.meta.env.VITE_BASE_URL_API
-    static API_KEY = import.meta.env.VITE_API_KEY
+    static BaseURL = "https://data-api.nftgo.io/"
+    static API_KEY = "d4ce7b45-d539-4b71-b6a6-c510e7a"
 }
